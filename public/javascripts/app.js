4a1b1ce6b429e933d7341c0ae714282e398f1904
Daniel <daniel_brena@outlook.com>
2015-05-25 11:10:52 -0500

    Revert f8efa5d..1478e57
    This rolls back to commit f8efa5d7b8a2cd6eb9181fc34eefd05a587f2b36.

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -5,6 +5,6 @@ Parse.initialize("feN1oQBjjIwhAm17ssUClGit9YzUGe3otyBQnuCu", "WCjSy03NpDIHs73xif
 
 var routeBus = angular.module('routeBus',[]);
 routeBus.constant('CONFIG',{
-   'URL':'http://stark-ridge-4075.herokuapp.com',
+   'URL':'https://stark-ridge-4075.herokuapp.com',
     'PORT':'80'
 });
--- public/javascripts/controller/controllerIndex.js
+++ public/javascripts/controller/controllerIndex.js
@@ -16,26 +16,17 @@ routeBus.controller('IndexCtrl',function($scope,socket,CONFIG){
         $scope.ubicacion.lat  = data.lat;
         $scope.ubicacion.lng = data.lng;
 
-        var image = {
-            url: CONFIG.URL + ":" + CONFIG.PORT + "/images/iconoAutobus.png",
-            // This marker is 20 pixels wide by 32 pixels tall.
-            size: new google.maps.Size(20, 32),
-            // The origin for this image is 0,0.
-            origin: new google.maps.Point(0,0),
-            // The anchor for this image is the base of the flagpole at 0,32.
-            anchor: new google.maps.Point(0, 32)
-        };
+
 
         $scope.marker = new google.maps.Marker({
             position: new google.maps.LatLng($scope.ubicacion.lat,$scope.ubicacion.lng),
             zIndex: google.maps.Marker.MAX_ZINDEX + 2,
-            animation: google.maps.Animation.DROP,
-            icon:image
+            animation: google.maps.Animation.DROP
         });
         $scope.marker.setMap($scope.mapa);
 
         //$scope.marker.setMap(null);
-        console.log(data.lat + data.lng);
+        console.log(data.lat);
     });
 
 
